ef963169e372ee271bc0e054354c175dae26a4c2
Erezfla <96227679+Erezfla@users.noreply.github.com>
2022-01-03 22:47:31 +0200

    Hi Opschool Team. Attached is my assignments. Hopefully it'll pass :)

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+
+ssh-keygen -t rsa -b 4096 -N ""  -C "server1 ssh key" -f  /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/
+
+
+ssh-keygen -t rsa -b 4096 -N ""  -C "server1 root ssh key" -f  ~/.ssh/id_rsa
+cp ~/.ssh/id_rsa.pub /vagrant/id_rsa.pub_s1_root
+
+cat /vagrant/id_rsa_s2.pub >> /home/vagrant/.ssh/authorized_keys
+
+cat /vagrant/id_rsa.pub_s2_root >> ~/.ssh/authorized_keys
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+cat /vagrant/id_rsa.pub_s1_root >> ~/.ssh/authorized_keys
+
+cp /vagrant/id_rsa_s2.pub /home/vagrant/.ssh/id_rsa.pub
+cp /vagrant/id_rsa_s2 /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chmod 600 /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+chmod 644 /home/vagrant/.ssh/id_rsa.pub
+
+
+
+cp /vagrant/id_rsa_s2_root ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+cp /vagrant/id_rsa.pub_s2_root ~/.ssh/id_rsa.pub
+chmod 644 ~/.ssh/id_rsa.pub
